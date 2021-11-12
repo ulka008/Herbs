199f0b1ef9d0d4e10031a72a2c07f1182fa94a80
ulka008 <77630428+ulka008@users.noreply.github.com>
2021-11-12 14:20:33 -0500

    updated date

--- countdown.py
+++ countdown.py
@@ -1,18 +1,6 @@
-from datetime import date, datetime
+from datetime import date
+f_date = date(2021, 11, 19)
+l_date = date(2022, 4, 1)
+delta = l_date - f_date
 
-x=datetime.today()
-y=x.replace(day=x.day+1, hour=3, minute=1, second=0)
-delta_t=y-x 
-secs=delta_t.seconds+1
-
-second = (secs % 60)
-minute = (secs / 60) % 60
-hour = (secs / 3600)
-day = (secs / 3600*24)
-
-print("Seconds : %s" % (second))
-print("Minute: %s" % (minute))
-print("Hour: %s" % (hour))
-print('Day: %s' % (day))
-
-print('Time left is %s:%s:%s') % (day, hour, minute, second)
+print(delta.days)
